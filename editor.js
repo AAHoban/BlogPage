13e88b428dcbfb941f9b477f31f8b89a7e5498e1
AAShah <megananospeed@gmail.com>
2022-02-03 04:02:44 +0300

    Confirm Button added
    Added a confirm button when users have seen their work and can confirm their work and publish it and adjusted some styling in editor page.

--- editor.js
+++ editor.js
@@ -9,9 +9,11 @@ function setup() {
 
     button.mousePressed(title);
     button.mousePressed(article);
+    button.mousePressed(publish_btn);
 
     gretting.input(titleTyping);
     input.input(articleTyping);
+    button.input(confirmingPublish);
 }
 
 function titleTyping() {
@@ -22,12 +24,23 @@ function articleTyping() {
     article.html(input.value());
 }
 
+function confirmingPublish() {
+    publish_btn.html(button.value());
+}
+
 function title() {
     var text = gretting.value();
-    var par = createP('<span style="font-size: 48px">' + text + '</span>');
+    var par = createP('<span style = "font-size: 48px">' + text + '</span>');
 }
 
 function article() {
     var text1 = input.value();
     var par = createP(text1);
+}
+
+function publish_btn() {
+    var btn = "Confirm Publish"
+    var par = createP(
+        '<button class = "confirmPublish">'+btn+'</button>',
+        );
 }
